fe10953bc48ad46956753c0459772731a6b495e5
imcarissa <carissa.sarreal@gmail.com>
2021-02-23 03:08:51 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 	 
 	Rails.application.routes.draw do
 	  resources :students, only: [:index, :show, :new, :create]
-	end
+  end
 
 
   # The priority is based upon order of creation: first created -> highest priority.
